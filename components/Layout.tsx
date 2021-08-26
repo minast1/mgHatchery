0c2b262479677d3d2dc932a1ec004e85b260df15
Edmond Marfo <edmarfo2@gmail.com>
2021-08-26 13:27:25 +0000

    remove drewer for better reponsiveness

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -142,23 +142,7 @@ export default function Layout() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List>
-      </Drawer>
+     
      
     </>
   );
