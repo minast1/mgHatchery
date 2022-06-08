60cac6185851ea5b3b967216d2a0d0de7fc00551
minast1 <edmarfo2@gmail.com>
2022-06-08 22:21:02 +0000

    remove shadow db and migrations

--- prisma/migrations/20211230005648_set_decimal_precision_for_rate_in_item_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `rate` on the `Item` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
-
-*/
--- AlterTable
-ALTER TABLE `Item` MODIFY `rate` DECIMAL(10, 2) NOT NULL;
